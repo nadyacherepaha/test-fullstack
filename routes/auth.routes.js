3cfb7fc5efe4736c0e3d86b3903b90ee56d734cd
nadyacherepaha <nad.cherepaha@gmail.com>
2021-05-17 13:08:19 +0300

    Add method 'registration'

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,40 @@
 const {Router} = require('express')
+const bcrypt = require('bcryptjs')
+const {check, validationResult} = require('express-validator')
+const User = require('../models/User')
 const router = Router()
 
-router.post('/register', async (req, res) => {
+router.post(
+    '/register',
+    [
+        check('email', 'Invalid email').isEmail(),
+        check('password', 'Minimum password length 6 characters')
+            .isLength({ min: 6 })
+    ],
+    async (req, res) => {
     try {
-        const {email, password} = req.body
+        const errors = validationResult(req)
 
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+                message: 'Incorrect registration data'
+            })
+        }
+
+        const {email, password} = req.body
+        const candidate = await User.findOne({ email })
+
+        if (candidate) {
+            return res.status(400).json({ message: 'This user already exists' })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12)
+        const user = new User({ email, password: hashedPassword })
+
+        await user.save()
+
+        res.status(201).json({ message: 'User are created' })
 
     } catch (e) {
         res.status(500).json({ message: 'Something went wrong'})
